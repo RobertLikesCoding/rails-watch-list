90d0bc196f03f9834ec2036a81e94d9a2577ffbf
Robert Baufeld <rbaufeld@posteo.de>
2024-05-24 17:25:00 +0200

    created the create action in comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,18 @@
 class CommentsController < ApplicationController
   def create
+    @list = List.find(params[:list_id])
+    @comment = @list.comments.build(comment_params)
+    
+    if @comment.save
+      redirect_to list_path(@list)
+    else
+      redirect_to list_path(@list), alert: 'There was an error creating the comment.'
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
   end
 end
